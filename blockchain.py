135a8b84131d77969f1229193ef2127e0e75bdfa
Rohan Jha <95472555+rohanjha04@users.noreply.github.com>
2023-04-16 14:36:56 +0000

    chain fixed

--- blockchain.py
+++ blockchain.py
@@ -18,8 +18,16 @@ class Block:
         self.hash= None
     
     def find_hash(self):
-        block_json_string= json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_json_string.encode()).hexdigest()
+        try:
+            block_json_string= json.dumps(self.__dict__, sort_keys=True)
+            return sha256(block_json_string.encode()).hexdigest()
+        except(TypeError):
+            if(list(self.transactions)):
+                x=(list(self.transactions)[0])
+                block_json_string= json.dumps([list(x), self.nonce])
+                return sha256(block_json_string.encode()).hexdigest()
+            block_json_string= json.dumps(list(self.transactions))
+            return sha256(block_json_string.encode()).hexdigest()
 
 class Blockchain:
     difficulty= 2
@@ -54,10 +62,10 @@ class Blockchain:
         return(block_hash.startswith('0'*Blockchain.difficulty) and block_hash== block.find_hash())
     
     def add_block(self, block, proof):
-        previous_hash= self.last_block.hash 
+        previous_hash= self.last_block().hash 
         if previous_hash!= block.previous_hash:
             return False
-        if not Blockchain.is_valid_proof(block, proof):
+        if not Blockchain.is_valid_proof(self, block, proof):
             return False
         
         block.hash= proof
@@ -70,11 +78,43 @@ class Blockchain:
     def mine(self):
         if not self.unconfirmed_transactions:
             return False
-        last_block= self.last_block
+        last_block= self.last_block()
         new_block= Block(id= last_block.id+1, transactions= self.unconfirmed_transactions, 
                         timestamp=time.time(), previous_hash= last_block.hash)
         proof= self.proof_of_work(new_block)
         self.add_block(new_block, proof)
         self.unconfirmed_transactions= []
         return new_block.id
-    
+
+chain= Blockchain()
+chain.create_genesis_block_set()
+result= chain.mine()
+print(chain.last_block().transactions)
+new_data= chain.last_block().transactions
+new_data.add(32)
+# print(new_data)
+# k= json.dumps(list(new_data))
+# print(sha256(k.encode()).hexdigest())
+# new_data.add(2344)
+# print(new_data)
+# new_data.add(2344)
+# print(new_data)
+print(type(new_data))
+chain.add_new_transaction(new_data)
+result= chain.mine()
+print(result)
+print(chain.last_block().transactions)
+new_data= chain.last_block().transactions[0]
+new_data.add(244853)
+chain.add_new_transaction(new_data)
+result= chain.mine()
+print(chain.last_block().transactions)
+print(result)
+new_data= chain.last_block().transactions[0]
+new_data.add(3142)
+chain.add_new_transaction(new_data)
+result= chain.mine()
+print(chain.last_block().transactions)
+print(result)
+last_block= chain.last_block().hash
+print(last_block)
