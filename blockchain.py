ea2ce26613593aaf15cbd7ccfc70ceefa50f44ef
Krish Agrawal <diwankrish.17@gmail.com>
2023-04-19 19:06:08 +0530

    blockchain update, new templates, permissions

--- blockchain.py
+++ blockchain.py
@@ -2,27 +2,29 @@
 The data format thought of 
 {
 "mail": "ee210002041@iiti.ac.in",
-"votes": "c1000c2000c3000......"
+"votes": "c1000,c2000,c3000......,"
 }
 """
 from hashlib import sha256
 import json
 import time
-class Block:
 
+class Block:
+    #Initialise block
     def __init__(self,id, transactions, timestamp, previous_hash):
         self.id= id
-        self.transactions= transactions
+        self.transactions= transactions #data of the block
         self.timesptamp= timestamp
         self.previous_hash= previous_hash
-        self.nonce= 0
+        self.nonce= 0 #facilitates POW
         self.hash= None
     
+    #Function to find hash of the block
     def find_hash(self):
         try:
             block_json_string= json.dumps(self.__dict__, sort_keys=True)
             return sha256(block_json_string.encode()).hexdigest()
-        except(TypeError):
+        except(TypeError): #For handling the set
             if(list(self.transactions)):
                 x=(list(self.transactions))
                 block_json_string= json.dumps([list(x), self.nonce])
@@ -31,16 +33,21 @@ class Block:
             return sha256(block_json_string.encode()).hexdigest()
 
 class Blockchain:
-    difficulty= 2
+
+    difficulty= 2 #Difficulty for POW. As it increases, POW takes more time
+    
+    #Initialise chain
     def __init__(self):
         self.chain= []
         self.unconfirmed_transactions= []
 
+    #Create the first block of the chain with "0" as previous hash and block id 0
     def create_genesis_block(self):
         genesis_block= Block(0,[],time.time(), "0")
         genesis_block.hash= genesis_block.find_hash()
         self.chain.append(genesis_block)
     
+    #If we want the data type for transaction to be set, use this function too create the genesis block
     def create_genesis_block_set(self):
         genesis_block= Block(0,{'start@iiti'},time.time(), "0")
         genesis_block.hash= genesis_block.find_hash()
@@ -48,9 +55,11 @@ class Blockchain:
     
 
     @property
+    #Access the last block of the chain
     def last_block(self):
         return self.chain[-1]
     
+    #Add block to the chain after verfying it has valid hash and no fiddling
     def add_block(self, block, proof):
         previous_hash= self.last_block.hash 
         if previous_hash!= block.previous_hash:
@@ -62,48 +71,48 @@ class Blockchain:
         return True
     
     @staticmethod
+    #Method to secure the chain further and reduce concurrency problems
     def proof_of_work(self, block):
-        block.nonce= 0
+        block.nonce= 0 #Used to get the required hash. It is changed to satisfy the condition of hash
         curr_hash= block.find_hash()
         while not curr_hash.startswith('0'*Blockchain.difficulty):
             block.nonce+=1
             curr_hash= block.find_hash()
         return curr_hash
     
+    #A new transaction is added here before mining it further
     def add_new_transaction(self, transaction):
         self.unconfirmed_transactions.append(transaction)
     
     @classmethod
+    #Check if hash has satisfied the proof of work or not
     def is_valid_proof(self, block, block_hash):
         return(block_hash.startswith('0'*Blockchain.difficulty) 
         and block_hash== block.find_hash())
 
     @classmethod
+    #Check the validity of the data in the chain to catch any malpractices
     def check_chain(self, chain):
         result= True
         previous_hash= chain[0].hash
         for block in chain[1:]:
-            print(block.transactions)
             block_hash= block.hash
-            print(block_hash)
             delattr(block, "hash")
             block_hash_now= block.find_hash()
             print(block_hash_now)
             if(block_hash_now!=block_hash):
-                print("HAA YAHIN SE AAYA")
                 return False
             if (not self.is_valid_proof(block, block_hash)) or (previous_hash!= block.previous_hash):
-                print('ho gaya')
                 return False
             block.hash, previous_hash= block_hash, block_hash
         return result
     
+    #Add new block to the chain
     def mine(self):
         if not self.unconfirmed_transactions:
             return False
         last_block= self.last_block
         for i in range(0, len(self.unconfirmed_transactions)):
-            # print(self.unconfirmed_transactions[i])
             new_block= Block(id= last_block.id+1, transactions= self.unconfirmed_transactions[i], 
                             timestamp=time.time(), previous_hash= last_block.hash)
             proof= self.proof_of_work(self, new_block)
@@ -111,55 +120,11 @@ class Blockchain:
         self.unconfirmed_transactions= []
         return True
     
-    def chk_result(self, chain, result):
-        for block in chain[1:]:
+    #Function to check the results of the election. Unrelated for basic functioning of blockchain
+    def chk_result(self, result):
+        for block in self.chain[1:]:
             x= block.transactions
-            votes= x["votes"]
+            votes= x["votes"].split(",")[:-1]
             for i in range(0, len(votes)):
                 result[votes[i]]= result[votes[i]]+1
         return result
-
-
-    
-# chain= Blockchain()
-# chain.create_genesis_block_set()
-# # chain.add_new_transaction(2123)
-# # result= chain.mine()
-# # chain.create_genesis_block_set()
-# # print(chain.last_block().transactions)
-# new_data= set(chain.last_block.transactions)
-# new_data.add(32)
-# # print(new_data)
-# # k= json.dumps(list(new_data))
-# # print(sha256(k.encode()).hexdigest())
-# # new_data.add(2344)
-# # print(new_data)
-# # new_data.add(2344)
-# # print(new_data)
-# # print(type(new_data))
-# chain.add_new_transaction(new_data)
-# # chain.add_new_transaction(234)
-# # # print(chain.unconfirmed_transactions)
-# result= chain.mine()
-# # # print(chain.unconfirmed_transactions)
-# # # print(chain.last_block().transactions)
-# new_data= set(chain.last_block.transactions)
-# new_data.add(32)
-# chain.add_new_transaction(new_data)
-# result= chain.mine()
-# # print((chain.chain[0].hash))
-# # # print(chain.last_block().transactions)
-# # # print(result)
-# # new_data= chain.last_block.transactions[0]
-# # new_data.add(3142)
-# # # chain.add_new_transaction(new_data)
-# # # result= chain.mine()
-# # # chain.add_new_transaction(new_data)
-# # # result= chain.mine()
-# # # print(result)
-# # # # print(chain.last_block().transactions)
-# # # print(chain.chain[2].hash)
-# # print(chain.chain[4].transactions)
-# chain.chain[1].transactions= {121342}
-# # # print(chain.chain)
-# print(chain.check_chain(chain.chain))
--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from bson import ObjectId
 import string
 import random
 import blockchain
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 app.secret_key = "hashvote"
@@ -24,63 +27,20 @@ flow = flow.Flow.from_client_secrets_file(
             'https://www.googleapis.com/auth/userinfo.profile', 'openid'],
     redirect_uri='http://localhost:5000/callback'
 )
-Blockchain_votes= blockchain.Blockchain()
-Blockchain_voter= blockchain.Blockchain()
+Blockchain_votes = blockchain.Blockchain()
+Blockchain_voter = blockchain.Blockchain()
 Blockchain_votes.create_genesis_block()
 Blockchain_voter.create_genesis_block_set()
-
-data = [
-    {
-        "id": "pres",
-        "name": "President",
-        "candidates": [
-            {
-              "id": "pres-1",
-              "name": "Candidate 1",
-            },
-            {
-                "id": "pres-2",
-                "name": "Candidate 2",
-            },
-            {
-                "id": "pres-3",
-                "name": "Candidate 3",
-            },
-            {
-                "id": "nota",
-                "name": "NOTA",
-            }
-        ]
-    },
-    {
-        "id": "vpres",
-        "name": "Vice President",
-        "candidates": [
-            {
-              "id": "vpres-1",
-              "name": "Candidate 1",
-            },
-            {
-                "id": "vpres-2",
-                "name": "Candidate 2",
-            },
-            {
-                "id": "vpres-3",
-                "name": "Candidate 3",
-            },
-            {
-                "id": "nota",
-                "name": "NOTA",
-            }
-        ]
-    },
-]
 
 client = pymongo.MongoClient(
     "mongodb+srv://diwankrish17:N4lTSO9A3DJ6sRYW@cluster0.wlsbebc.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 
 admin_ids = ['ee210002041@iiti.ac.in']
+
+start_time = datetime(2021, 4, 1, 0, 0, 0, 0)
+end_time = datetime(2021, 4, 2, 0, 0, 0, 0)
+
 
 def unique_id(size):
     chars = list(set(string.ascii_uppercase +
@@ -120,7 +80,7 @@ def role():
     if "google_id" not in session:
         return redirect("/")
     else:
-        prev= set(Blockchain_voter.last_block.transactions)
+        prev = set(Blockchain_voter.last_block.transactions)
         if session["email"] in admin_ids:
             return redirect("/admin")
         elif session.get("email") in prev:
@@ -132,7 +92,7 @@ def role():
 @app.route('/vote')
 @login_is_required
 def vote_candidate():
-    prev= set(Blockchain_voter.last_block.transactions)
+    prev = set(Blockchain_voter.last_block.transactions)
     if session.get("email") in prev:
         return redirect("/already")
     positions = list(db.positions.find({}))
@@ -142,7 +102,8 @@ def vote_candidate():
         pos_dict["id"] = position["_id"]
         pos_dict["name"] = position["name"]
         pos_dict["candidates"] = []
-        candidates = list(db.candidates.find({"position": str(position["_id"])}))
+        candidates = list(db.candidates.find(
+            {"position": str(position["_id"])}))
         for candidate in candidates:
             candidate_dict = {}
             candidate_dict["id"] = candidate["uid"]
@@ -152,9 +113,11 @@ def vote_candidate():
     print(data_list)
     return render_template('voting.html', data=data_list)
 
+
 @app.route('/already')
 def already():
     return render_template('already.html')
+
 
 @app.route('/login/google')
 def login_google():
@@ -208,7 +171,7 @@ def thanks():
 
 @app.route('/voting', methods=['POST'])
 def voting():
-    prev= set(Blockchain_voter.last_block.transactions)
+    prev = set(Blockchain_voter.last_block.transactions)
     data = request.form
     if session.get("email") in prev:
         return redirect('/already')
@@ -219,9 +182,11 @@ def voting():
         votes = ""
         for key, value in data.items():
             votes += value + ","
-        Blockchain_votes.add_new_transaction({"mail": session.get("email"), "votes": votes})
+        Blockchain_votes.add_new_transaction(
+            {"mail": session.get("email"), "votes": votes})
         Blockchain_votes.mine()
     return redirect('/thanks')
+
 
 @app.route('/getchain', methods=['GET'])
 def getchain():
@@ -230,6 +195,7 @@ def getchain():
     print(Blockchain_votes.chain[1].transactions)
     print(Blockchain_voter.chain[1].transactions)
     return redirect('/')
+
 
 @app.route('/add/candidate', methods=['POST'])
 def add_candidate():
@@ -252,7 +218,10 @@ def add_position():
     elif session["email"] not in admin_ids:
         return abort(403)
     data = request.form
-    position_data = {"name": data["name"]}
+    perms = data.getlist("permission")
+    if "all" in perms:
+        perms = ["all"]
+    position_data = {"name": data["name"], "permission": perms}
     db.positions.insert_one(position_data)
     return redirect('/positions')
 
@@ -264,6 +233,7 @@ def add_voter():
     elif session["email"] not in admin_ids:
         return abort(403)
     data = request.form
+    
     voter_data = {"name": data["name"], "email": data["email"],
                   "branch": data["branch"], "voted": "false"}
     db.voters.insert_one(voter_data)
@@ -280,7 +250,8 @@ def get_positions():
     pos_list = []
     for pos in positions:
         pos_list.append(pos)
-    return render_template('positions.html', positions=pos_list)
+    branch_list = list(db.branch.find())
+    return render_template('positions.html', positions=pos_list, branch=branch_list)
 
 
 @app.route('/candidates')
@@ -308,12 +279,32 @@ def get_voters():
         return redirect('/')
     elif session["email"] not in admin_ids:
         return abort(403)
-    voters = db.voters.find()
-    voters_list = []
-    for voter in voters:
-        voters_list.append(voter)
-    return render_template('voters.html', voters=voters_list)
+    voters_list = list(db.voters.find())
+    branch_list = list(db.branch.find())
+    return render_template('voters.html', voters=voters_list, branch=branch_list)
 
+@app.route('/branch', methods=['GET', 'POST'])
+def branch():
+    if "google_id" not in session:
+        return redirect('/')
+    elif session["email"] not in admin_ids:
+        return abort(403)
+    if request.method == 'POST':
+        data = request.form
+        db.branch.insert_one({"name": data["name"], "categories": []})
+    print(list(db.branch.find()))
+    return render_template('branch.html', branches=list(db.branch.find()))
+
+@app.route('/category', methods=['POST'])
+def category():
+    if "google_id" not in session:
+        return redirect('/')
+    elif session["email"] not in admin_ids:
+        return abort(403)
+    data = request.form
+    print(data)
+    db.branch.update_one({"_id": ObjectId(data["branch"])}, {"$push": {"categories": data["category"]}})
+    return redirect('/branch')
 
 @app.route('/delete/position/<id>')
 def delete_position(id):
@@ -344,6 +335,84 @@ def delete_voter(id):
         return abort(403)
     db.voters.delete_one({"_id": ObjectId(id)})
     return redirect('/voters')
+
+def func(pct, allvalues):
+    absolute = int(pct / 100.*np.sum(allvalues))
+    return "{:.1f}%\n({:d})".format(pct, absolute)
+
+@app.route('/visualise')
+def visualise():
+    plot_data = {}
+    positions = list(db.positions.find())
+    for pos in positions:
+        plot_data[pos["name"]] = {}
+        candidates = list(db.candidates.find({"position": str(pos["_id"])}))
+        list_of_win = {}
+        for candi in candidates:
+            list_of_win[candi["name"]] = 5
+        plot_data[pos["name"]] = list_of_win
+    # fig, axs = plt.subplots(2,2)
+    # i= 0
+    # j= 0
+    # count= 1
+    
+    for pos, pos_data in plot_data.items():
+        # print(pos_data)
+        explode = []
+        count= 0
+        for i in range(0, len(pos_data.keys())):
+            explode.append(count)
+            count= count+ 0.05
+        plt.pie(list(pos_data.values()), autopct = lambda pct: func(pct,list(pos_data.values())), labels=list(pos_data.keys()), explode= explode, shadow= True)
+        plt.title(pos)
+        # if(count%2==0):
+        #     i=i+1
+        # else:
+        #     j=j+1
+        # count= count+1
+        plt.savefig('./fig.png')
+        plt.show() 
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    if "google_id" not in session:
+        return redirect('/')
+    elif session["email"] not in admin_ids:
+        return abort(403)
+    candidates = db.candidates.find()
+    candidates_map = {}
+    for candidate in candidates:
+        candidates_map[candidate['uid']] = 0
+    Blockchain_votes.chk_result(candidates_map)
+    requests = []
+    for candidate_uid, votes in candidates_map.items():
+        requests.append(pymongo.UpdateOne(
+            {"uid": candidate_uid}, {"$set": {"votes": votes}}))
+    db.candidates.bulk_write(requests)
+    return candidates_map, 200
+
+
+@app.route('/timeset', methods=['POST'])
+def timeset():
+    if "google_id" not in session:
+        return redirect('/')
+    # elif session["email"] not in admin_ids:
+    #     return abort(403)
+    data = request.form
+    print(data)
+    global start_time
+    global end_time
+    start_time = datetime.strptime(data['start'], '%Y-%m-%dT%H:%M')
+    end_time = datetime.strptime(data['end'], '%Y-%m-%dT%H:%M')
+    return redirect('/dashboard')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if "google_id" not in session:
+        return redirect('/')
+    return render_template('dashboard.html', start=start_time, end=end_time)
 
 
 if __name__ == '__main__':
