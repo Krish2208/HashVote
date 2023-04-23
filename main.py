8283ed84a56aa635896c030b71602995e4a5002f
Krish Agrawal <diwankrish.17@gmail.com>
2023-04-23 13:17:07 +0530

    adds comments

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ import numpy as np
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+# Initializes a flask app
 app = Flask(__name__)
+
+# Set .env file to store the secrets
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -31,20 +34,27 @@ flow = flow.Flow.from_client_secrets_file(
             'https://www.googleapis.com/auth/userinfo.profile', 'openid'],
     redirect_uri=os.environ.get("REDIRECT_URI")
 )
+
+# Initializes the Blockchain of Voters and Votes
 Blockchain_votes = blockchain.Blockchain()
 Blockchain_voter = blockchain.Blockchain()
 Blockchain_votes.create_genesis_block()
 Blockchain_voter.create_genesis_block_set()
 
+# Connect to MongoDB
 client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
 db = client.test
 
+# Set the Admin IDs
 admin_ids = ['ee210002041@iiti.ac.in',
              'cse210001083@iiti.ac.in', 'cse210001034@iiti.ac.in']
 
+# Default start and end time of the elections
 start_time = datetime(2021, 4, 1, 0, 0, 0, 0)
 end_time = datetime(2023, 5, 2, 0, 0, 0, 0)
 
+
+# Schema Rules for MongoDB
 if "voters" not in db.list_collection_names():
     db.create_collection("voters")
 if "positions" not in db.list_collection_names():
@@ -147,12 +157,15 @@ branch_validator = {
 db.command("collMod", "branch", validator=branch_validator)
 
 
+# Function to generate random UID for the candidates
+# of the given input size
 def unique_id(size):
     chars = list(set(string.ascii_uppercase +
                  string.digits).difference('LIO01'))
     return ''.join(random.choices(chars, k=size))
 
 
+# Wrapper function for checking if user is logged in
 def login_is_required(function):
     def wrapper(*args, **kwargs):
         cur_time = datetime.now()
@@ -166,6 +179,7 @@ def login_is_required(function):
     return wrapper
 
 
+# Wrapper function for checking if user is admin
 def admin_is_required(function):
     def admin_wrap(*args, **kwargs):
         if "google_id" not in session:
@@ -178,6 +192,7 @@ def admin_is_required(function):
     return admin_wrap
 
 
+# Home Page of the App
 @ app.route('/')
 def index():
     if "google_id" in session:
@@ -185,6 +200,7 @@ def index():
     return render_template('login.html')
 
 
+# Route to assign the role to the user after login
 @ app.route('/role')
 @ login_is_required
 def role():
@@ -201,6 +217,7 @@ def role():
         return redirect("/vote")
 
 
+# Google Login Route
 @ app.route('/login/google')
 def login_google():
     authorization_url, state = flow.authorization_url()
@@ -210,6 +227,7 @@ def login_google():
     return redirect(authorization_url)
 
 
+# Logout the user
 @ app.route("/logout")
 @ login_is_required
 def logout():
@@ -217,6 +235,7 @@ def logout():
     return redirect("/")
 
 
+# Callback URL after the user is logged in from the google oauth
 @ app.route('/callback')
 def callback():
     flow.fetch_token(authorization_response=request.url)
@@ -241,6 +260,7 @@ def callback():
     return redirect("/role")
 
 
+# Check if the voting is still going on or not
 @ app.route('/notime')
 def notime():
     cur_time = datetime.now()
@@ -249,12 +269,15 @@ def notime():
     return render_template('notime.html')
 
 
+# Check if the user is not added to the voter list
 @ app.route('/notvoter')
 @ login_is_required
 def notvoter():
     return render_template('notvoter.html')
 
 
+# Main Voting route which will check if the authenticated user has already voted or not
+# After checking it will render the frontend page with all the candidates and respective positions
 @ app.route('/vote')
 @ login_is_required
 def vote_candidate():
@@ -281,18 +304,21 @@ def vote_candidate():
     return render_template('voting.html', data=data_list)
 
 
+# If the user has already voted they will be redirected to this URL, with an easter egg
 @ app.route('/already')
 @ login_is_required
 def already():
     return render_template('already.html')
 
 
+# After the user has voted, this page will be rendered to thank the user
 @ app.route('/thanks')
 @ login_is_required
 def thanks():
     return render_template('thanks.html')
 
 
+# Voting POST method gets the user votes and adds it into the chain and mine a new block
 @ app.route('/voting', methods=['POST'])
 @ login_is_required
 def voting():
@@ -313,12 +339,15 @@ def voting():
     return redirect('/thanks')
 
 
+# Admin homepage route
 @ app.route('/dashboard')
 @ admin_is_required
 def dashboard():
     return render_template('dashboard.html', start=start_time, end=end_time)
 
 
+# Admin positions page route, it render the frontend with all currently added positions
+# It also defines a POST method which takes in the admin input for new position to add
 @ app.route('/positions', methods=['GET', 'POST'])
 @ admin_is_required
 def get_positions():
@@ -341,6 +370,8 @@ def get_positions():
     return render_template('positions.html', positions=pos_list, branch=branch_list)
 
 
+# Admin candidates page route, it render the frontend with all currently added candidates
+# It also defines a POST method which takes in the admin input for new candidates to add
 @ app.route('/candidates', methods=['GET', 'POST'])
 @ admin_is_required
 def get_candidates():
@@ -364,6 +395,8 @@ def get_candidates():
     return render_template('candidates.html', positions=pos_list, candidates=candidates_list)
 
 
+# Admin voters page route, it render the frontend with all currently added voters
+# It also defines a POST method which takes in the admin input for new voter to add
 @ app.route('/voters', methods=['GET', 'POST'])
 @ admin_is_required
 def get_voters():
@@ -378,6 +411,8 @@ def get_voters():
     return render_template('voters.html', voters=voters_list, branch=branch_list)
 
 
+# Admin branch page route, it render the frontend with all currently added branch and categories in each
+# It also defines a POST method which takes in the admin input for new branch to add
 @ app.route('/branch', methods=['GET', 'POST'])
 @ admin_is_required
 def branch():
@@ -387,6 +422,7 @@ def branch():
     return render_template('branch.html', branches=list(db.branch.find()))
 
 
+# It defines a POST method which takes in the admin input for new category to add
 @ app.route('/category', methods=['POST'])
 @ admin_is_required
 def category():
@@ -396,6 +432,7 @@ def category():
     return redirect('/branch')
 
 
+# It also defines a POST method which takes in the admin input for deleting an already added position
 @ app.route('/delete/position/<id>', methods=['post'])
 def delete_position(id):
     if "google_id" not in session:
@@ -407,6 +444,7 @@ def delete_position(id):
     return redirect('/positions')
 
 
+# It also defines a POST method which takes in the admin input for deleting an already added candidate
 @ app.route('/delete/candidate/<id>', methods=['post'])
 def delete_candidate(id):
     if "google_id" not in session:
@@ -417,6 +455,7 @@ def delete_candidate(id):
     return redirect('/candidates')
 
 
+# It also defines a POST method which takes in the admin input for deleting an already added voter
 @ app.route('/delete/voter/<id>', methods=['post'])
 def delete_voter(id):
     if "google_id" not in session:
@@ -427,6 +466,7 @@ def delete_voter(id):
     return redirect('/voters')
 
 
+# It also defines a POST method which takes in the admin input for deleting an already added category
 @ app.route('/delete/category/<id>/<category>', methods=['post'])
 def delete_category(id, category):
     if "google_id" not in session:
@@ -438,6 +478,7 @@ def delete_category(id, category):
     return redirect('/branch')
 
 
+# It also defines a POST method which takes in the admin input for editing an already added voter
 @ app.route('/edit/voter/<id>', methods=['post'])
 def edit_voter(id):
     if "google_id" not in session:
@@ -450,11 +491,13 @@ def edit_voter(id):
     return redirect('/voters')
 
 
+# It defines a function to style the visualisation charts
 def func(pct, allvalues):
     absolute = int(pct / 100.*np.sum(allvalues))
     return "{:.1f}%\n({:d})".format(pct, absolute)
 
 
+# It defines a route for geting the visualisation charts(Currently Unintegrated)
 @ app.route('/visualise')
 @ admin_is_required
 def visualise():
@@ -481,6 +524,7 @@ def visualise():
         plt.show()
 
 
+# It defines a POST method which takes in the admin input for publishing the result
 @ app.route('/publishresult', methods=['POST'])
 @ admin_is_required
 def publishresult():
@@ -505,6 +549,7 @@ def publishresult():
     return redirect('/')
 
 
+# It defines a method which displays result to authenticated users
 @ app.route('/result')
 @ login_is_required
 def result():
@@ -524,6 +569,7 @@ def result():
     return render_template('result.html', candidates=data_list)
 
 
+# It defines a POST method which takes in the admin input for defining the voting timing
 @ app.route('/timeset', methods=['POST'])
 @ admin_is_required
 def timeset():
@@ -539,6 +585,7 @@ def timeset():
     return redirect('/dashboard')
 
 
+# It defines a POST method which takes in the admin input for verifing the blockchain of votes
 @ app.route('/checkchain', methods=['POST'])
 @ admin_is_required
 def checkchain():
